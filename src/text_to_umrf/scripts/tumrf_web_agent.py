a4fb02d0cfeb1a968d304c280229693a87cea077
RValner <valnerrobert@gmail.com>
2019-09-09 04:21:39 +0300

    Update

--- src/text_to_umrf/scripts/tumrf_web_agent.py
+++ src/text_to_umrf/scripts/tumrf_web_agent.py
@@ -13,15 +13,23 @@ def callback(data):
     r = requests.post('http://ec2-3-94-111-251.compute-1.amazonaws.com', data=nl_chatter)
     print(r.status_code, r.reason)
     json_tumrf_str = r.text #.encode("utf-8")
+    print json_tumrf_str
     json_tumrf = json.loads(json_tumrf_str)
-    json_tumrf['name'] = 'Nameless'
     json_tumrf['suffix'] = '0'
+    graph_name = ""
+
+    if json_tumrf['input_parameters']['verb']['pvf_value'] == 'stop':
+        json_tumrf['name'] = 'TaStop'
+        graph_name = "Stop Graph"
+    else:
+        json_tumrf['name'] = 'TaDrive'
+        graph_name = "Drive Graph"
     
     wake_word = json_tumrf.get('wakeword').encode('ascii', 'ignore')
     del json_tumrf['wakeword']
 
     graph_msg = UmrfJsonGraph()
-    graph_msg.graph_name = 'Web Agent Graph'
+    graph_msg.graph_name = graph_name
     graph_msg.targets.append(wake_word)
     graph_msg.umrf_json_strings.append(json.dumps(json_tumrf))
     tumrf_pub.publish(graph_msg)
